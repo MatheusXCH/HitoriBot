2de94f463f529b11143139bfacf5a511c909ae5c
Matheus Henrique da Cunha Xavier <73074043+MatheusXCH@users.noreply.github.com>
2021-02-17 21:04:23 -0300

    Add files via upload

--- bin/settings.py
+++ bin/settings.py
@@ -1,8 +1,8 @@
 #GLOBAL PATHS:
 
 #Path para 'Rules'
-rule_path = './bin/regras/'
+rule_path = './regras/'
 #Path para 'Stickers'
-image_path = './bin/stickers/'
+image_path = './stickers/'
 #Path para 'Help'
-help_path = './bin/help/'
+help_path = './help/'
