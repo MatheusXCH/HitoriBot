a52dfde258775816b51cca898b63c9d92847cd39
Matheus Henrique da Cunha Xavier <matheus.xavier.ismart@gmail.com>
2021-03-12 16:45:13 -0300

    StoreSteam Cog Updated

--- codes/steamcontent/steambigpicture.py
+++ codes/steamcontent/steambigpicture.py
@@ -1,16 +1,23 @@
-import os, dotenv, requests, json, pprint
+import os, dotenv, requests, json, pprint, random
 from dotenv import load_dotenv
 from pprint import pprint
 import steam
 
 import codes.settings as st
 
+
+load_dotenv()
+my_steam_id = '76561198058563121'
+steam_key = os.getenv('STEAM_KEY')
 class SteamBigPicture:
+    #EMOJIS
+    EMOJI_METACRITIC = '<:metacritic_logo:819722763492261898>'
+    
     
     def __init__(self):
         self.get_all_games()
         
-    def load_all_games(self, source = 'steam'):
+    def get_all_games(self, source = 'steam'):
         '''Read data from 'steam_data.txt'
         
         source - 'steam' or 'steamspy'
@@ -24,7 +31,7 @@ class SteamBigPicture:
                 data = json.load(json_file)
             return data
     
-    def get_all_games(self, source = 'steam'):
+    def request_all_games(self, source = 'steam'):
         '''Requests new data from Steam/SteamSpy and update the '.txt' file
         
         source - 'steam' or 'steamspy'
@@ -40,10 +47,8 @@ class SteamBigPicture:
             
             #Page 0 to 9 - 10000 itens mais comprados da Steam, segundo o SteamSpy
             for i in range(10):
-                print(f'Contador: {i} ')
                 payload = {'request': 'all', 'page':i}
                 response_all_steamspy_apps = requests.get('https://steamspy.com/api.php', params=payload)
-                pprint(response_all_steamspy_apps.url)
                 if i == 0:
                     all_steamspy_apps = response_all_steamspy_apps.json()
                 else:
@@ -55,36 +60,34 @@ class SteamBigPicture:
     def get_game_info(self, game_title, source = 'steam'):
         '''Get game info
         
-        game_title: <str>
+        game_title: str\n
         source: 'steam' or 'steamspy'
         '''
         if source == 'steam':
-            all_games_json = self.load_all_games('steam')
+            all_games_json = self.get_all_games('steam')
             all_games_list = all_games_json['applist']['apps']
             
             game_info = next((item for item in all_games_list if item['name'] == game_title), None)
             app_id = game_info['appid']
             
             #Request to https://store.steampowered.com/api/appdetails/'
-            payload = {'appids' : app_id, 'filters' : keys}
+            payload = {'appids' : app_id, 'l':'brazilian'}
             response_game_info = requests.get('https://store.steampowered.com/api/appdetails/', params=payload)
             game_info = response_game_info.json()
             return game_info[str(app_id)]['data']
         
         elif source == 'steamspy':
-            all_games_steamspy_json = self.load_all_games('steamspy')
+            all_games_steamspy_json = self.get_all_games('steamspy')
             app_id_steamspy = next((item for item in all_games_steamspy_json if all_games_steamspy_json[item]['name'] == game_title), None)
             
             #Request to steamspy.com/api.php?
             payload = {'request':'appdetails', 'appid':app_id_steamspy}
             response_steamspy_game_info = requests.get('https://steamspy.com/api.php', params=payload)
-            print(response_steamspy_game_info.url)
             game_info_steamspy = response_steamspy_game_info.json()
             return game_info_steamspy
-        
+    
+    #TODO Request All Free Games
 
 # steam = SteamBigPicture()
-# game_dict = steam.load_all_games(source='steamspy')
-# game_info = steam.get_game_info_steamspy('Hollow Knight')
-
-# pprint(game_info)
+# game = steam.get_game_info('Hollow Knight', 'steamspy')
+# pprint(game)
